4c51108616a9435d1e90ea4268210875a3bb3cc9
SEDoug <doug.macgregor@gmail.com>
2020-03-25 17:51:12 -0500

    adding script folder

---
+++ scripts/ipsweep.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## SEDoug Multiple Ping Sweeper ##
+#  ////////////////////////////  #
+# & - allows threading (multiple lines) #
+# cut -d (delimiter) -f field tr (translate) # 
+
+if [ "$1" == "" ]
+then
+echo "Your forgot an IP address!"
+echo "Symtax: ./ipsweep.sh 192.168.1"
+
+else
+for ip in `seq 1 254`; do
+ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+done
+fi
