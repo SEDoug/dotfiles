2766012ed23eb65788afcc4e97520f5f3b15ce74
Doug MacGregor <doug.macgregor@gmail.com>
2020-11-07 15:55:17 -0600

    Update object.py

--- object.py
+++ object.py
@@ -7,3 +7,5 @@ bbox, label, conf = cv.detect_common_objects(im)
 output_image = draw_bbox(im, bbox, label, conf)
 plt.imshow(output_image)
 plt.show()
+
+# https://towardsdatascience.com/object-detection-with-less-than-10-lines-of-code-using-python-2d28eebc5b11
