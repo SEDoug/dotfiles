dcbfa82d390749084212a15b65ef36bfd829000d
sedoug <doug.macgregor@gmail.com>
2021-03-11 21:13:00 -0700

    updated script with crontab info

--- scripts/update-pihole.sh
+++ scripts/update-pihole.sh
@@ -5,3 +5,6 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y autoremove
 pihole -up
+
+# Add to crontab
+# 33 3 * * * sudo /bin/bash /home/<username>/Scripts/update-pihole.sh > /home/<username>/Logs/update-pihole.log
